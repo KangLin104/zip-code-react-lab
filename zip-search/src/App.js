6c59854037e48267a85c7619898bb0048e262386
kang lin <kanglin104@gmail.com>
2021-10-17 10:36:46 -0400

    kang lin zip lab

--- zip-search/src/App.js
+++ zip-search/src/App.js
@@ -1,28 +1,71 @@
 import React, { Component } from 'react';
 import './App.css';
 
+function City( {data} ){
+  return(<div className='city-container'>
+    <div className='city'>
+      {data.City},{data.State}
+    </div>
+    
+    <ul >
 
-function City(props) {
-  return (<div>This is the City component</div>);
+      <li>State: {data.State}</li>
+      <li>Location: ({data.Long},{data.Lat})</li>
+      <li>Population (Estimated): {data.EstimatedPopulation}</li>
+      <li>Total Wage: {data.TotalWages}</li>
+      
+    </ul>
+  </div>)
 }
 
-function ZipSearchField(props) {
-  return (<div>This is the ZipSearchField component</div>);
+function ZipSearchField(props){
+  return (
+    <div className='zip-input'>
+      Zip Code:
+      <input type="text" onChange={props.changeHandler}/>
+    </div>
+  )
 }
 
 
 class App extends Component {
+  state = {
+    zipCode:'',
+    cities: [],
+  }
+  zipChanged= (event) =>{
+    console.log(event.target.value );
+    this.setState({zipcode: event.target.value} )
+
+    fetch(`http://ctp-zip-api.herokuapp.com/zip/${event.target.value}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data)
+        this.setState({cities:data})
+      })
+      .catch(err =>{
+        console.log('not result')
+        this.setState({cities:[]})
+      })
+      
+  }
+
   render() {
+
     return (
+      
       <div className="App">
         <div className="App-header">
           <h2>Zip Code Search</h2>
         </div>
-        <ZipSearchField />
-        <div>
-          <City />
-          <City />
+        <div >
+          <ZipSearchField zipCode={this.state.zipCode} changeHandler={this.zipChanged} />
+          <div className="App">
+            {this.state.cities.map((city) => <City data={city}/>)}
+         
+          </div>
         </div>
+        
       </div>
     );
   }
